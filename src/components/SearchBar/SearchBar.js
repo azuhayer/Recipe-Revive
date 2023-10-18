4a63dc38a0d86f8cadf433d8a4e35916c65cd754
Riaz Ahmed <00.riaz.ahmed@gmail.com>
2023-10-17 22:08:04 -0400

    start of search component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+      <div className="flex p-3 bg-white border rounded-lg shadow-lg shadow-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:shadow-gray-900/[.2]">
+        <input
+          type="text"
+          id="search-recipe"
+          className="p-3 mr-[10px] w-full border rounded-md focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:text-gray-400"
+          placeholder="Search recipe"
+        />
+
+        <button className="p-4 inline-flex items-center rounded-md border font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </button>
+      </div>
+    );
+  };
+  
+  export default SearchBar;
