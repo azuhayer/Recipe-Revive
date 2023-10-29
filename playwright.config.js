8202bd0bbd0ba461e9a6064155028a21539fd6dc
NikhilPrajith <57269214+NikhilPrajith@users.noreply.github.com>
2023-10-28 22:08:13 -0400

    ci: added github actions

--- playwright.config.js
+++ playwright.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   // Glob patterns or regular expressions that match test files.
+  
   testMatch: 'tests/E2ETest/*',
+  webServer:{
+    command: "npm run test:start",
+    url:'http://localhost:3000',
+  }
 });
