ead9a354edbf1857556d525429802be45f5b9ed6
Riaz Ahmed <00.riaz.ahmed@gmail.com>
2023-11-11 19:00:17 -0500

    updates to details page and components, along with signup css adjustment

--- src/components/RecipeView/RecipeView.js
+++ src/components/RecipeView/RecipeView.js
@@ -4,31 +4,39 @@ import styles from './RecipeView.module.css';
 
 function RecipeView() {
   return (
-    <div className={styles.page}>
-      <h1 className={styles.centerStyle}>Easy Mushroom Soup</h1>
-      <h2 className={styles.centerStyle}>40 Min | 928g Nutrition | 200 Reviews</h2>
-      <img
-        src="https://www.recipetineats.com/wp-content/uploads/2021/04/Mushroom-Soup-in-bowl.jpg"
-        alt="Mushroom Soup"
-        className={styles.image}
-      />
+    <div className= {`bg-slate-900 min-h-[100vh] h-[auto]  ${styles.page}`}>
+        <div className='flex lg:flex-row flex-col justify-center'>
+          <div className='text-center lg:mr-14'>
+            <h1 className='text-[40px] text-white'>Easy Mushroom Soup</h1>
+            <h2 className='text-[20px] text-white '>40 Min | 928g Nutrition | 200 Reviews</h2>
+            <img
+              src="https://www.recipetineats.com/wp-content/uploads/2021/04/Mushroom-Soup-in-bowl.jpg"
+              alt="Mushroom Soup"
+              className='mx-auto block rounded-full lg:h-[500px] lg:w-[500px] md:h-[350px] md:w-[350px] h-[280px] w-[280px]'
+            />
+          </div>
 
-      <div className={styles.flexContainerStyle}>
-        <div className={styles.flexItemStyle}>
-          <h3>Ingredients</h3>
-            <ul className={styles.list}>
-              <li>1 tbsp olive oil</li>
-              <li>1 onion, finely chopped</li>
-              <li>750g chestnut mushrooms, sliced</li>
-              <li>2 garlic cloves, crushed</li>
-              <li>500ml vegetable stock</li>
-            </ul>
+          <div className='flex flex-col justify-center align-middle text-black'>
+            <div className='border rounded-[10px] shadow p-10 md:mx-auto m-[20px] bg-white sm:max-w-[400px]'>
+              <h3 className='text-[35px] text-center text-decoration-line: underline'>Ingredients</h3>
+              <div>
+                <ol className={`list-disc text-[20px] mt-4 p-2`}>
+                  <li>1 tbsp olive oil</li>
+                  <li>1 onion, finely chopped</li>
+                  <li>750g chestnut mushrooms, sliced</li>
+                  <li>2 garlic cloves, crushed</li>
+                  <li>500ml vegetable stock</li>
+                </ol>
+              </div>
+              <div className='flex justify-center'>
+                <a target="_blank" href="https://google.com/" rel="">
+                    <div className='mt-10 px-4 text-white bg-black hover:bg-white hover:text-black py-2 text-[20px] rounded-full text-center'>View instructions</div>
+                </a>
+              </div>
+            </div>
+            
+          </div>
         </div>
-        <div className={styles.flexItemStyle}>
-          <h3>Link to Recipe</h3>
-          <p>url</p>
-        </div>
-      </div>
     </div>
   );
 }
