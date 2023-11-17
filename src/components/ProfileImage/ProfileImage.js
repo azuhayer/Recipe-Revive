2f3f749f57fce9f8dce32cccb98eddb62663c3f6
NikhilPrajith <57269214+NikhilPrajith@users.noreply.github.com>
2023-11-16 20:29:44 -0500

    Updated userProfile page

--- src/components/ProfileImage/ProfileImage.js
+++ src/components/ProfileImage/ProfileImage.js
@@ -1,13 +1,21 @@
+'use client'
 import React from 'react';
 import styles from './ProfileImage.module.css';
+import { useState } from 'react';
 
 // User profile icon placeholder
 const profileImage = "assets/profile.png";
 
 export default function ProfileImage() {
+  const images = ["https://images.pexels.com/photos/4457125/pexels-photo-4457125.jpeg?auto=compress&cs=tinysrgb&w=1200",
+"https://images.pexels.com/photos/5908188/pexels-photo-5908188.jpeg?auto=compress&cs=tinysrgb&w=1200","https://images.pexels.com/photos/9424930/pexels-photo-9424930.jpeg?auto=compress&cs=tinysrgb&w=1200",
+"https://images.pexels.com/photos/784633/pexels-photo-784633.jpeg?auto=compress&cs=tinysrgb&w=1200"]
+
+  const rand = Math.floor(Math.random() * images.length);
+
   return (
     <div className={styles.profileImage}>
-      <img src={profileImage} alt="User Profile" />
+      <img src={images[rand]} alt="User Profile" />
     </div>
   );
 }
