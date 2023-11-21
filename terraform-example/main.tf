9d668ca042d3898fcb2edeb2205a3058575f5899
Kazi Sadman <ksadman000@citymail.cuny.edu>
2023-11-21 01:12:14 -0500

    terraform commit

---
+++ terraform-example/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    vercel = {
+      source = "vercel/vercel"
+      version = "~> 0.3"
+    }
+  }
+}
+resource "vercel_project" "example" {
+  name      = "recipe-revive"
+  framework = "nextjs"
+  git_repository = {
+    type = "github"
+    repo = "FreshPineapple-jpg/Recipe-Revive"
+  }
+}
+provider "vercel" {
+  api_token = "uWwDIOugTHm32oe76oyMJDxQ"
+}
